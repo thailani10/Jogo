c90dbce542aaba19b83fb01d5588195f7dee3736
thailani10 <thailaniferreira.03@gmail.com>
2026-06-22 16:47:35 -0300

    Update Mario game-over image source path

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = 'assets/imgs/game-over.png';
+        mario.src = 'game-over.png';
         mario.style.width = '160px';
         mario.style.marginLeft = '130px';
 
@@ -74,7 +74,7 @@ const restart = () => {
             mario.style.animation = 'none';
             mario.style.bottom = `${marioPosition}px`;
     
-            mario.src = 'assets/imgs/game-over.png';
+            mario.src = 'game-over.png';
             mario.style.width = '160px';
             mario.style.marginLeft = '130px';
 
